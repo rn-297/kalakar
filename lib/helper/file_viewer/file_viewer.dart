4af832a58002a81fee26be0b8b091471aa4fd5de
Deepshikha-sudesi <deepshikha@sudesi.in>
2024-07-26 18:18:35 +0530

    file viewer

---
+++ lib/helper/file_viewer/file_viewer.dart
@@ -0,0 +1,178 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:appinio_video_player/appinio_video_player.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:pdfx/pdfx.dart';
+import 'package:video_player/video_player.dart';
+import '../../helper/route_helper.dart';
+
+class ViewFilePage extends StatefulWidget {
+  const ViewFilePage({Key? key}) : super(key: key);
+
+  @override
+  State<ViewFilePage> createState() => _ViewFilePageState();
+}
+
+class _ViewFilePageState extends State<ViewFilePage> {
+  RxString fileType = ''.obs;
+  RxBool isInitialized = false.obs;
+  int count = 0;
+  late PdfControllerPinch pdfController;
+  late VideoPlayerController videoController;
+  late CustomVideoPlayerController customVideoPlayerController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    // setState(() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    try {
+      videoController.dispose();
+    } catch (e) {
+      // print(e);
+    }
+    try {
+      pdfController.dispose();
+    } catch (e) {
+      // print(e);
+    }
+    super.dispose();
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size(double.infinity, 64.h),
+        child: AppBar(title: Text("Rohan"),),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+
+          Expanded(
+            child: Obx(() => fileType.value != ""
+                ? Container(
+              color: Colors.blue,
+              padding: EdgeInsets.symmetric(horizontal: 8.w),
+              child: fileType.value == "pdf"
+                  ? PdfViewPinch(
+                controller: pdfController,
+                onDocumentError: (e) {
+                  // print(e);
+                },
+                onDocumentLoaded: (val) {
+                  // print(val);
+                },
+                scrollDirection: Axis.vertical,
+              )
+                  : fileType.value == "mp4"
+                  ? isInitialized.value
+                  ? AspectRatio(
+                aspectRatio: 1 / 1,
+                child: CustomVideoPlayer(
+                  customVideoPlayerController:
+                  customVideoPlayerController,
+                ),
+              )
+                  : Center( child: CircularProgressIndicator())
+                  : Center(
+                child: AspectRatio(
+                    aspectRatio: 10 / 16,
+                    child: Image.file(
+                        File(''))),
+              ),
+            )
+                : Container()),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> setVideoData() async {
+    print("object");
+    final String _localFilePath = "";
+    final File _localFile = File(_localFilePath);
+    try {
+      videoController = await VideoPlayerController.file(_localFile)
+        ..initialize().then((value) => {
+          setState(() {}),
+        });
+
+      // customVideoPlayerController = await CustomVideoPlayerController(
+      //     context: context,
+      //     videoPlayerController: videoController,
+      //     customVideoPlayerSettings: CustomVideoPlayerSettings());
+      setState(() {
+        isInitialized.value = true;
+        // print("Done");
+      });
+    } catch (e) {
+      // print(e);
+    }
+    setState(() {
+      // print("1Object");
+    });
+  }
+
+  Future<void> checkFileType() async {
+    fileType.value = "";
+
+    // print("set pdf image video");
+
+    fileType.refresh();
+
+    fileType.value = "folderController.fileViewPath"
+        .toString()
+        .split("/")
+        .last
+        .split(".")
+        .last
+        .replaceAll("'", "");
+    try {
+      // });
+      // print(folderController.fileViewPath.lengthSync());
+      // print("fileType${fileType.value} ${folderController.fileViewPath.path}");
+      print(fileType.value);
+      if (fileType.value == "pdf") {
+        if (count == 0) {
+          pdfController = PdfControllerPinch(
+              document:
+              PdfDocument.openFile("folderController.fileViewPath.path"));
+
+          count++;
+        } else {
+          // await pdfController.document.then((value) => value.close());
+          pdfController.loadDocument(
+              PdfDocument.openFile("folderController.fileViewPath.path"));
+        }
+        Timer(const Duration(milliseconds: 100), () {
+          setState(() {
+            fileType.refresh();
+          });
+        });
+        // pdfController.document =
+        //     PdfDocument.openFile(folderController.fileViewPath.path);
+      } else if (fileType.value == 'mp4') {
+        setVideoData();
+      } else {}
+    } catch (e) {
+      // print(e);
+    }
+
+    // print(fileType.value);
+    fileType.refresh();
+  }
+
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,15 +6,19 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
+import device_info_plus
 import file_selector_macos
 import path_provider_foundation
+import pdfx
 import share_plus
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
